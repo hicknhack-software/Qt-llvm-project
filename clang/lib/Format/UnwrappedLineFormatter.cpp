9b992a0f7f160dd6c75f20a4dcfcf7c60a4894df
Ivan Donchevskii <ivan.donchevskii@qt.io>
2018-10-12 08:19:25 +0200

    [clang-format] Introduce the flag which allows not to shrink lines
    https://reviews.llvm.org/D53072
    
    Currently there's no way to prevent to lines optimization even
    if you have intentionally put <CR> to split the line.
    
    In general case it's fine. So I would prefer to have such option
    which you can enable in special cases (for me it's an IDE related use case).
    
    Revert this change if upstream clang-format offers better solution.
    
    Reviewed-by; Marco Bubke <marco.bubke@qt.io>
    
    Cherry picked from commit
    clang.git/fa1b9053729ec6a4425a44ec5502dd388928274a.
    
    Change-Id: I92296dd177c811593ebba7d4b5c3c5f472af10a2
    Reviewed-by: Cristian Adam <cristian.adam@qt.io>

--- clang/lib/Format/UnwrappedLineFormatter.cpp
+++ clang/lib/Format/UnwrappedLineFormatter.cpp
@@ -741,7 +741,7 @@ public:
   LineFormatter(ContinuationIndenter *Indenter, WhitespaceManager *Whitespaces,
                 const FormatStyle &Style,
                 UnwrappedLineFormatter *BlockFormatter)
-      : Indenter(Indenter), Whitespaces(Whitespaces), Style(Style),
+      : Indenter(Indenter), Style(Style), Whitespaces(Whitespaces),
         BlockFormatter(BlockFormatter) {}
   virtual ~LineFormatter() {}
 
@@ -782,7 +782,8 @@ protected:
       // assert so that we can simply call this function for all tokens.
       return true;
 
-    if (NewLine) {
+    if (NewLine || (Previous.Children[0]->First->MustBreakBefore &&
+                    Style.KeepLineBreaksForNonEmptyLines)) {
       int AdditionalIndent = State.Stack.back().Indent -
                              Previous.Children[0]->Level * Style.IndentWidth;
 
@@ -827,10 +828,10 @@ protected:
   }
 
   ContinuationIndenter *Indenter;
+  const FormatStyle &Style;
 
 private:
   WhitespaceManager *Whitespaces;
-  const FormatStyle &Style;
   UnwrappedLineFormatter *BlockFormatter;
 };
 
@@ -853,7 +854,7 @@ public:
     while (State.NextToken) {
       bool Newline =
           Indenter->mustBreak(State) ||
-          (Indenter->canBreak(State) && State.NextToken->NewlinesBefore > 0);
+          (State.NextToken->NewlinesBefore > 0 && Indenter->canBreak(State));
       unsigned Penalty = 0;
       formatChildren(State, Newline, /*DryRun=*/false, Penalty);
       Indenter->addTokenToState(State, Newline, /*DryRun=*/false);
